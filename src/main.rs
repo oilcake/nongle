8e3c1e270d998fa6e49295bf204b96d7f4ab57b8
oilcake <oilpie@gmail.com>
2023-03-22 04:31:09 +0300

    file name parsing added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use std::fs;
+
 mod convert;
-mod que;
 mod performer;
+mod que;
 
 fn main() {
     let amplitude: f64 = 0.07593;
-    let bounds = convert::VelocityBounds{
+    let bounds = convert::VelocityBounds {
         top: 1.0,
-        bottom: 0.0
+        bottom: 0.0,
     };
     let velocity = convert::amplitude_to_velocity(amplitude, bounds);
     println!("velocity is {}", velocity);
@@ -18,7 +20,18 @@ fn main() {
         que.next_in_que();
         println!("and now id in que is {}", que.q_id)
     }
+
+    let paths = fs::read_dir("./Xy_samples/35_B2_").unwrap();
+
+    let mut names: Vec<String> = vec![];
+    for path in paths {
+        names.push(path.unwrap().path().display().to_string());
+    }
+    for name in names {
+        println!("{}", name);
+        println!("{:?}", performer::mello::note_analyzer(&name));
+    }
     // define a dummy note
-    let sample = performer::mello::Mello::new('e'.to_string());
-    sample.play();
+    // let sample = performer::mello::Mello::new('e'.to_string());
+    // sample.play();
 }
