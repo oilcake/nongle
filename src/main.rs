568be9db06e5a703cfee08fd40a6f08357250395
oilcake <oilpie@gmail.com>
2024-05-17 20:24:30 +0300

    working midi in example added to main

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,68 @@
-use std::fs;
+use std::error::Error;
+use std::io::{stdin, stdout, Write};
 
-mod convert;
-mod performer;
-mod que;
+use midir::{Ignore, MidiInput};
 
 fn main() {
-    let amplitude: f64 = 0.07593;
-    let bounds = convert::VelocityBounds {
-        top: 1.0,
-        bottom: 0.0,
+    match run() {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+
+    let mut midi_in = MidiInput::new("midir reading input")?;
+    midi_in.ignore(Ignore::None);
+
+    // Get an input port (read from console if multiple are available)
+    let in_ports = midi_in.ports();
+    let in_port = match in_ports.len() {
+        0 => return Err("no input port found".into()),
+        1 => {
+            println!(
+                "Choosing the only available input port: {}",
+                midi_in.port_name(&in_ports[0]).unwrap()
+            );
+            &in_ports[0]
+        }
+        _ => {
+            println!("\nAvailable input ports:");
+            for (i, p) in in_ports.iter().enumerate() {
+                println!("{}: {}", i, midi_in.port_name(p).unwrap());
+            }
+            print!("Please select input port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            in_ports
+                .get(input.trim().parse::<usize>()?)
+                .ok_or("invalid input port selected")?
+        }
     };
-    let velocity = convert::amplitude_to_velocity(amplitude, bounds);
-    println!("velocity is {}", velocity);
 
-    let mut que: que::Que = que::Que::new(7);
-    let mut count: u8 = 0;
-    while count < 10 {
-        count += 1;
-        que.next_in_que();
-        println!("and now id in que is {}", que.q_id)
-    }
+    println!("\nOpening connection");
+    let in_port_name = midi_in.port_name(in_port)?;
 
-    let paths = fs::read_dir("./Xy_samples/35_B2_").unwrap();
+    // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
+    let _conn_in = midi_in.connect(
+        in_port,
+        "midir-read-input",
+        move |stamp, message, _| {
+            println!("{}: {:?} (len = {})", stamp, message, message.len());
+        },
+        (),
+    )?;
 
-    let mut names: Vec<String> = vec![];
-    for path in paths {
-        names.push(path.unwrap().path().display().to_string());
-    }
-    for name in names {
-        println!("{}", name);
-        println!("{:?}", performer::mello::note_analyzer(&name));
-    }
-    // define a dummy note
-    // let sample = performer::mello::Mello::new('e'.to_string());
-    // sample.play();
+    println!(
+        "Connection open, reading input from '{}' (press enter to exit) ...",
+        in_port_name
+    );
+
+    input.clear();
+    stdin().read_line(&mut input)?; // wait for next enter key press
+
+    println!("Closing connection");
+    Ok(())
 }
