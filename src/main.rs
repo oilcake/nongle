870285c73b13e1d67c62606ccad654137ff70c5f
oilcake <oilpie@gmail.com>
2024-09-01 13:34:14 +0300

    some garbage removed, now always play from port 0, IAC Driver

--- src/main.rs
+++ src/main.rs
@@ -27,31 +27,12 @@ fn run() -> Result<(), Box<dyn Error>> {
 
     // Get an input port (read from console if multiple are available)
     let in_ports = midi_in.ports();
-    let in_port = match in_ports.len() {
-        0 => return Err("no input port found".into()),
-        1 => {
-            println!(
-                "Choosing the only available input port: {}",
-                midi_in.port_name(&in_ports[0]).unwrap()
-            );
-            &in_ports[0]
-        }
-        _ => {
-            println!("\nAvailable input ports:");
-            for (i, p) in in_ports.iter().enumerate() {
-                println!("{}: {}", i, midi_in.port_name(p).unwrap());
-            }
-            print!("Please select input port: ");
-            stdout().flush()?;
-            let mut input = String::new();
-            stdin().read_line(&mut input)?;
-            in_ports
-                .get(input.trim().parse::<usize>()?)
-                .ok_or("invalid input port selected")?
-        }
-    };
-
-    println!("\nOpening connection");
+    println!("Available input ports:");
+    for (i, p) in in_ports.iter().enumerate() {
+        println!("{}: {}", i, midi_in.port_name(p).unwrap());
+    }
+    let in_port = &in_ports[0];
+    println!("\nOpening connection on port {}", midi_in.port_name(in_port)?);
     let in_port_name = midi_in.port_name(in_port)?;
 
     let (tx, rx) = mpsc::channel::<MidiNote>();
@@ -96,7 +77,8 @@ fn run() -> Result<(), Box<dyn Error>> {
     // which represents pitch and velocity
     // to encapsulate it in a struct
     while let Ok(note) = rx.recv() {
-        println!("pitch {}, and velocity {}", note.pitch, note.velocity);
+        print!("\rpitch {}, and velocity {}", note.pitch, note.velocity);
+        let _ = std::io::stdout().flush();
         // Now you can clone and use memory_sound multiple times
         controller.add(template_sound.clone());
         // do not know what is it
